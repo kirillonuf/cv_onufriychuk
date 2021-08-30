0635afe72abe46cf83449a39a75f6a7fc1bcad71
our ame <onufk1989@gmail.com>
2021-08-30 22:53:37 +0300

    sfg

--- src/App.js
+++ src/App.js
@@ -6,7 +6,21 @@ import PersonalInfo from './components/PersonalInfo';
 import LifePath from './components/LifePath';
 import { Links } from './components/Links';
 
-const App = () => {
+var doc = new jsPDF();
+var specialElementHandlers = {
+    '#editor': function (element, renderer) {
+        return true;
+    }
+};
+export const save =  () =>{
+    doc.fromHTML(document.querySelector(".general").html(), 15, 15, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+    doc.save('cv-onufriychuk.pdf');
+};
+
+ const App = () => {
 
 
 
