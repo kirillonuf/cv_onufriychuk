970f214a4f65b8e63ce9988ecd7eee8a2c3cb191
our ame <onufk1989@gmail.com>
2021-08-31 10:24:22 +0300

    пр

--- src/App.js
+++ src/App.js
@@ -6,19 +6,6 @@ import PersonalInfo from './components/PersonalInfo';
 import LifePath from './components/LifePath';
 import { Links } from './components/Links';
 
-var doc = new jsPDF();
-var specialElementHandlers = {
-    '#editor': function (element, renderer) {
-        return true;
-    }
-};
-export const save =  () =>{
-    doc.fromHTML(document.querySelector(".general").html(), 15, 15, {
-        'width': 170,
-            'elementHandlers': specialElementHandlers
-    });
-    doc.save('cv-onufriychuk.pdf');
-};
 
  const App = () => {
 
--- src/components/LifePath.js
+++ src/components/LifePath.js
@@ -2,14 +2,42 @@
 
 import Education from "./Education";
 import Skills from "./Skills";
+import { jsPDF } from "jspdf";
+
+
+
+// Default export is a4 paper, portrait, using millimeters for units
+// const save = new jsPDF();
+
+// save.text("Hello world!", 10, 10);
+// save.save("a4.pdf");
+
+   var doc = new jsPDF('p', 'pt', 'letter');
+var specialElementHandlers = {
+    '#editor': function (element, renderer) {
+        return true;
+    }
+};
+
+ const save =  () =>{
+ 
+    doc.text(document.body.innerHTML, 15, 15, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+   
+    doc.save('cv-onufriychuk.pdf');
+    console.log("SAVE");
+};
 
 const LifePath = (props) => {
     const data = props.data;
+   
     return (
 
         <div className="lifePath">
 <div className="header"> <div className="vertSeparator"></div>
-<input className="btn"   translate="no" value="Save"  type="button"></input></div>
+<input className="btn" onClick={save}  translate="no" value="Save"  type="button"></input></div>
            
 
             <div className="separator2"></div>
