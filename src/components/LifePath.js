8c5c596a06a117ab5425cb3db9fe5d5b8d3c6cf0
our ame <onufk1989@gmail.com>
2021-09-02 20:54:29 +0300

    fgf

--- src/components/LifePath.js
+++ src/components/LifePath.js
@@ -3,12 +3,41 @@ import Education from "./Education";
 import Skills from "./Skills";
 import html2canvas from 'html2canvas';
 import { jsPDF } from "jspdf";
+// import *  from 'jspdf';
+ import domtoimage from 'dom-to-image';
+
+
+
+// // https://www.npmjs.com/package/jspdf
+// // https://www.npmjs.com/package/dom-to-image
+// // import * as jsPDF from 'jspdf';
+// // import domtoimage from 'dom-to-image';
+
+// const copy=()=>{
+//    // const div = document;
+//     const options = { background: 'white', height: 845, width: 595 };
+//     domtoimage.toPng(document.body, options).then((canvas) => {
+//         //Initialize JSPDF
+//         const doc = new jsPDF('p', 'mm', 'a4');
+//         //Add image Url to PDF
+//         doc.addImage(canvas, 'PNG', 0, 0, 210, 340);
+//         doc.save('pdfDocument.pdf');
+//     })
+// }
+
+
+
+
 const copy = () => {
     const url = document.URL;
     console.log(url);
 //   document.body.appendChild()
 
-    html2canvas(document.body,{scale:10} ).then(function (canvas) {
+    html2canvas(document.body,{
+
+        scale:10
+
+    } ).then(function (canvas) {
      
         const screenWidth = window.screen.width;
         const screenHeight = window.screen.height;
@@ -37,13 +66,13 @@ let left=0;
       
 if(screenWidth > screenHeight){
     
-    width=270;
+    width=310;
     height=210;
     // left=20;
     // top=0;
 }else{
     width=210;
-    height=270;
+    height=310;
     // left=0;
     // top=20;
 }
