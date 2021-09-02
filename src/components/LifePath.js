5ad523d6b9fefde5f5f9cda45d96bfa1bb1ca11b
our ame <onufk1989@gmail.com>
2021-09-02 19:09:59 +0300

    screen

--- src/components/LifePath.js
+++ src/components/LifePath.js
@@ -4,26 +4,51 @@ import Skills from "./Skills";
 import html2canvas from 'html2canvas';
 import { jsPDF } from "jspdf";
 const copy = () => {
-
+    const url = document.URL;
+    console.log(url);
+//   document.body.appendChild()
 
     html2canvas(document.body).then(function (canvas) {
 
         const screenWidth = window.screen.width;
         const screenHeight = window.screen.height;
+        const url = document.URL;
 
-        var docPDF = new jsPDF({
+let width=0;
+let height=0;
+let top=0;
+let left=0;
+        let docPDF = new jsPDF(
+             {
 
-            orientation: screenWidth > screenHeight ? "l" : "p",
+             orientation: screenWidth > screenHeight ? "l" : "p",
 
-            unit:"px",
+             unit: "mm",
 
-            format: [screenWidth, screenHeight]
-   
-        });
+            format: "a4",
+            compress: true,
+            precision: 1,
+            hotfixes: ['px_scaling']
+            // [screenWidth, screenHeight]
 
-        docPDF.addImage(canvas, 'JPEG', 0, 0, screenWidth, screenHeight);
-
-        docPDF.output('save','cv-onufriychuk.pdf');
+         }
+        );
+      
+if(screenWidth > screenHeight){
+    
+    width=270;
+    height=210;
+    left=20;
+    top=0;
+}else{
+    width=220;
+    height=270;
+    left=0;
+    top=20;
+}
+        docPDF.addImage( canvas.con, 'JPEG',left,top,width,height);
+        // , 'JPEG', 0, 0,screenWidth, screenHeight
+        docPDF.output('save', 'cv-onufriychuk.pdf');
 
     });
 
